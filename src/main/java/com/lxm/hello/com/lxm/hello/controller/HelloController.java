6408470eeaf7d40d61185f1852f1889f5260ec5d
刘孝明 <liuxiaoming@epipechina.com>
2018-08-23 19:25:09 +0800

    创建hello项目

---
+++ src/main/java/com/lxm/hello/com/lxm/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.lxm.hello.com.lxm.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public  String hello(){
+    return "现在的时间是："+new Date();
+    }
+
+}
