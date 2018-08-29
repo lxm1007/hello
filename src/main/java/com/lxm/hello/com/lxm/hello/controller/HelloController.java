15d675baf5682f2c2793ef3dcb207fce120da4cb
lxm <15853261424@163.com>
2018-08-29 22:14:02 +0800

    测试钩子

--- src/main/java/com/lxm/hello/com/lxm/hello/controller/HelloController.java
+++ src/main/java/com/lxm/hello/com/lxm/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.lxm.hello.com.lxm.hello.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import java.util.Date;
 @RestController
 public class HelloController {
 
+    @Autowired
+    HelloService helloService;
+
     @RequestMapping("/hello")
     public  String hello(){
-    return "现在的时间是："+new Date();
+    return "现在的时间是："+this.helloService.foormatDate(new Date());
     }
 
     @RequestMapping("/")
---
+++ src/main/java/com/lxm/hello/com/lxm/hello/controller/HelloService.java
@@ -0,0 +1,16 @@
+package com.lxm.hello.com.lxm.hello.controller;
+
+import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class HelloService {
+
+    public String foormatDate(Date date){
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(date);
+    }
+}
